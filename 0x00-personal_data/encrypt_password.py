e39e6f4df3e74a6b566a44b6a91f62d67b920da2
josfam <jamukun@proton.me>
2024-09-01 05:25:30 +0300

    Implement `6. Check valid password`

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,9 +4,16 @@
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """Salts, hashes and returns the hashed version of the provided password"""
     as_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()  # salt
     hashed_pwd = bcrypt.hashpw(as_bytes, salt)
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Returns True if the provided password matches the hash,
+    False otherwise"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
