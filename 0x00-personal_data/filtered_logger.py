a11eea726abe0b2491801d6ac9a6fb6c87ce0c2a
josfam <jamukun@proton.me>
2024-09-01 04:28:58 +0300

    Implement `3. Connect to secure database`

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,10 @@
 """Using re.sub to redact messages"""
 
 import re
+import os
 import logging
+import mysql.connector
+from mysql.connector import connection
 from typing import List, Tuple
 
 PII_FIELDS: Tuple[str] = ('email', 'phone', 'ssn', 'password', 'name')
@@ -46,3 +49,16 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db():
+    """Connects to a mysql database, using environment variables"""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = ose.getenv('PERSONAL_DATA_DB_NAME')
+
+    cnx = mysql.connector.connect(
+        user=username, password=password, host=db_host, database=db_name
+    )
+    return cnx
