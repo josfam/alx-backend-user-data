0fcd26a158e88370d5793a94c20612f7deb8e36e
josfam <jamukun@proton.me>
2024-09-04 02:41:58 +0300

    Implement `6. Basic auth`

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
-import os
 
 
 app = Flask(__name__)
@@ -19,6 +19,12 @@ if auth == 'auth':
     from api.v1.auth.auth import Auth
 
     auth = Auth()
+elif auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+
+    auth = BasicAuth()
+else:
+    pass
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""For basic authentication"""
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """For basic authentication"""
+
+    pass
