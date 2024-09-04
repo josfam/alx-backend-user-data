89d8079a0c687b430d214bb701f81ce2a22f821a
josfam <jamukun@proton.me>
2024-09-04 03:05:48 +0300

    Docs: Add docstring

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
         self, authorization_header: str
     ) -> str:
+        """Returns the base64 part of the authorization header"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
