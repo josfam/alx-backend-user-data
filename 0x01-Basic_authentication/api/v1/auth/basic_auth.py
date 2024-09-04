ca6ba744586ec48a496c5df498b1df4d2607ee18
josfam <jamukun@proton.me>
2024-09-04 04:14:11 +0300

    Implement `9. Basic - User credentials`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,17 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             return None
         return decoded
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Extracts the users email and password from the
+        authorization header
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if not ':' in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
