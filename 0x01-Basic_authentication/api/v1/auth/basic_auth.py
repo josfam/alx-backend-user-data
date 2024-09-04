2ef752f0f48f4c20ba858718274bb35eefa21947
josfam <jamukun@proton.me>
2024-09-04 04:01:30 +0300

    Implement `8. Basic - Base64 decode`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 
 """For basic authentication"""
 
+import base64
 from .auth import Auth
 
 
@@ -19,3 +20,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Returns the decoded value of the Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        # should be valid Base64
+        try:
+            decoded = base64.b64decode(base64_authorization_header).decode(
+                'utf-8'
+            )
+        except base64.binascii.Error:
+            return None
+        return decoded
