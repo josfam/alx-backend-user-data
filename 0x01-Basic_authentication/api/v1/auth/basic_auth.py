11726790aadd3992f11706faef61a5f7a39e055e
josfam <jamukun@proton.me>
2024-09-08 15:56:03 +0300

    Formatting: Fix column 80

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,8 +57,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar('User'):
-        """Returns a user object that matched the user_email, if they have a matching
-        password as well. Returns None otherwise"""
+        """Returns a user object that matched the user_email,
+        if they have a matching password as well. Returns None otherwise"""
         if user_email is None or (not isinstance(user_email, str)):
             return None
         if user_pwd is None or (not isinstance(user_pwd, str)):
